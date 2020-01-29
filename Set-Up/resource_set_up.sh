8cff46d4677b1433e8796350cbcd295172323532
Jonathan Scholtes <joscholt@microsoft.com>
2020-01-29 17:24:14 -0600

    set-up

--- Set-Up/resource_set_up.sh
+++ Set-Up/resource_set_up.sh
@@ -6,17 +6,9 @@ select yn in "Yes" "No"; do
         No ) break;;
     esac
 done
-
 az login
-
 RESOURCE_GROUP=rg-FarmBeatLab-$RANDOM
 IOT_HUB=IotFarmBeatLab-$RANDOM
-
 az group create --name RESOURCE_GROUP --location "Central US"
-
 az iot hub create --name IOT_HUB \
    --resource-group RESOURCE_GROUP --sku S1
-
-
-
-
