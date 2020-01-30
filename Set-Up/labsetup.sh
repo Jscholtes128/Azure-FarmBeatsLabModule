e609156f10a0439b18cc464e53ee6d41c95dac59
Jonathan Scholtes <joscholt@microsoft.com>
2020-01-30 13:29:56 -0600

    updated install scripts

--- Set-Up/labsetup.sh
+++ Set-Up/labsetup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 RESOURCE_GROUP=rg-FarmBeatLab-$RANDOM
 IOT_HUB=IotFarmBeatLab-$RANDOM
-echo 's/<ADD DEVICE CONNECTION STRING HERE/'"$IOT_HUB"'/g'
+DEVICE_ID=FarmBeatEdgeDevice
 az login
 az group create --name $RESOURCE_GROUP --location "Central US"
 az iot hub create --name $IOT_HUB \
    --resource-group $RESOURCE_GROUP --sku S1
-CONNECTION_STR=`az iot hub device-identity create --device-id FarmBeatEdgeDevice --hub-name "$IOT_HUB" --edge-enabled`
-sudo sed -i 's/<ADD DEVICE CONNECTION STRING HERE/'"$CONNECTION_STR"'/g' /etc/iotedge/config.yaml
+az iot hub device-identity create --device-id $DEVICE_ID --hub-name $IOT_HUB --edge-enabled
+CONNECTION_STR=`az iot hub device-identity show-connection-string --device-id "$DEVICE_ID" --hub-name "$IOT_HUB"
+echo "$CONNECTION_STR"
+#sudo sed -i 's/<ADD DEVICE CONNECTION STRING HERE/'"$CONNECTION_STR"'/g' /etc/iotedge/config.yaml
