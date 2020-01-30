db02eec5f341ff5d8d5d26dc6c555d3909850669
Jonathan Scholtes <joscholt@microsoft.com>
2020-01-30 13:48:28 -0600

    bug

--- Set-Up/routesetup.sh
+++ Set-Up/routesetup.sh
@@ -1,5 +1,5 @@
 az extension add --name azure-cli-iot-ext
-IOT_HUB=IotFarmBeatLab-7199
+IOT_HUB=IotFarmBeatLab-23335
 DEVICE_ID=FarmBeatEdgeDevice
 #echo "Type the year that you want to check (4 digits), followed by [ENTER]:"
 #read year
