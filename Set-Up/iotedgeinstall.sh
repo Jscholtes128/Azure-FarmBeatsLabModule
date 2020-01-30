2386daacfb51453d92a32817ec219b8029a907c4
Jonathan Scholtes <joscholt@microsoft.com>
2020-01-30 10:39:56 -0600

    bash

--- Set-Up/iotedgeinstall.sh
+++ Set-Up/iotedgeinstall.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 sudo apt-get install libssl1.0.2
-curl https://packages.microsoft.com/config/debian/10/prod.list > ./microsoft-prod.list
-sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo apt-get update
-sudo apt update --allow-releaseinfo-change
-sudo apt-get upgrade
-sudo apt-get install moby-engine
-sudo apt-get install moby-cli
-sudo apt-get update
-sudo apt-get install iotedge
+curl https://packages.microsoft.com/config/debian/stretch/multiarch/prod.list > ./microsoft-prod.list && \
+sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/ && \
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && \
+sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/ && \
+sudo apt-get update && \
+sudo apt-get -y install iotedge
 sudo nano /etc/iotedge/config.yaml
