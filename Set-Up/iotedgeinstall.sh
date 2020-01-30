0983e07e6647270e46a261199bc768239901b209
Jonathan Scholtes <joscholt@microsoft.com>
2020-01-30 10:23:13 -0600

    fix for buster

--- Set-Up/iotedgeinstall.sh
+++ Set-Up/iotedgeinstall.sh
@@ -5,6 +5,8 @@ sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo apt-get update
+sudo apt update --allow-releaseinfo-change
+sudo apt-get upgrade
 sudo apt-get install moby-engine
 sudo apt-get install moby-cli
 sudo apt-get update
