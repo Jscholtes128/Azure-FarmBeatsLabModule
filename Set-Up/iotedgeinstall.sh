d3f5156f4c8248290c3c570dfb59fd9eb0e5870a
Jonathan Scholtes <joscholt@microsoft.com>
2020-01-30 10:19:38 -0600

    updated for buster

--- Set-Up/iotedgeinstall.sh
+++ Set-Up/iotedgeinstall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-curl https://packages.microsoft.com/config/debian/stretch/multiarch/prod.list > ./microsoft-prod.list
+#curl https://packages.microsoft.com/config/debian/stretch/multiarch/prod.list > ./microsoft-prod.list
+curl https://packages.microsoft.com/config/debian/10/prod.list > ./microsoft-prod.list
 sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
