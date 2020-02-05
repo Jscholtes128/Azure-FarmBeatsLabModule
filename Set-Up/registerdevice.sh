afa38e72a540a7771457251e00bf48d817232f46
Jonathan Scholtes <joscholt@microsoft.com>
2020-02-05 14:45:03 -0600

    bug

--- Set-Up/registerdevice.sh
+++ Set-Up/registerdevice.sh
@@ -13,4 +13,4 @@ curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/
 curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/sensormodelpressure.json > /templates
 curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/sensormodeltemp.json > /templates
 
-python3 sensorregistration.py --endpoint $ENDPOINT--client_id $CLIENT_ID --tenent_id $CLIENT_SECRET --client_secret $TENANT_ID
+python3 sensorregistration.py --endpoint $ENDPOINT--client_id $CLIENT_ID --tenent_id $TENANT_ID --client_secret $CLIENT_SECRET
