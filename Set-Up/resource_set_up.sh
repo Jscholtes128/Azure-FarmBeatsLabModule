1af03c6f71cdc052c9841cb4f2909bc6a8cc8203
Jonathan Scholtes <jscholtes128@gmail.com>
2020-02-05 12:11:34 -0600

    Update resource_set_up.sh

--- Set-Up/resource_set_up.sh
+++ Set-Up/resource_set_up.sh
@@ -19,16 +19,17 @@ az storage account create \
     --location $LOCATION \
     --sku Standard_LRS 
 
-az storage container create --name install
+az storage container create --account-name $STORE --name install
 
 curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/iotedgeinstall.sh > iotedgeinstall.sh
 
 sed -i 's/<CONNECTION>/"${CONNECTION_STR}"/g' iotedgeinstall.sh
 
 az storage blob upload \
+    --account-name $STORE
     --container-name install \
     --name iotedgeinstall.sh \
     --file iotedgeinstall.sh
 
 end=`date -u -d "60 minutes" '+%Y-%m-%dT%H:%MZ'`
-az storage blob generate-sas -c install -n iotedgeinstall.sh --permissions r --expiry $end --https-only
+az storage blob generate-sas --account-name $STORE -c install -n iotedgeinstall.sh --permissions r --expiry $end --https-only
