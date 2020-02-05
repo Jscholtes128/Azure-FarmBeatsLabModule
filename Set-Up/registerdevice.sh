fda8286388eeb3344b02ac4ae76a31632420eaf3
Jonathan Scholtes <joscholt@microsoft.com>
2020-02-05 14:54:26 -0600

    str concat

--- Set-Up/registerdevice.sh
+++ Set-Up/registerdevice.sh
@@ -14,6 +14,6 @@ curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/
 curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/sensormodelpressure.json > templates/sensormodelpressure.json
 curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/sensormodeltemp.json > templates/sensormodeltemp.json
 
-echo '--endpoint "${ENDPOINT}"--client_id "${CLIENT_ID}" --tenent_id "${TENANT_ID}" --client_secret "${CLIENT_SECRET}"')
+echo "--endpoint "${ENDPOINT}"--client_id "${CLIENT_ID}" --tenent_id "${TENANT_ID}" --client_secret "${CLIENT_SECRET}""
 
 python3 sensorregistration.py --endpoint $ENDPOINT--client_id $CLIENT_ID --tenent_id $TENANT_ID --client_secret $CLIENT_SECRET
