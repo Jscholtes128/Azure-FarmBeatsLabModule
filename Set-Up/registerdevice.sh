fcbabecefa56d427799cee0b6f228459fcb68784
Jonathan Scholtes <jscholtes128@gmail.com>
2020-02-05 14:37:10 -0600

    Update registerdevice.sh

--- Set-Up/registerdevice.sh
+++ Set-Up/registerdevice.sh
@@ -8,10 +8,10 @@ curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/
 
 mkdir templates
 
-curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/device.json > /templates/device.json
-curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/devicemodel.json > /templates/devicemodel.json
-curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/sensor.json > /templates/sensor.json
-curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/sensormodelpressure.json > /templates/sensormodelpressure.json
-curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/sensormodeltemp.json > /templates/sensormodeltemp.json
+curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/device.json > templates/device.json
+curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/devicemodel.json > templates/devicemodel.json
+curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/sensor.json > templates/sensor.json
+curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/sensormodelpressure.json > templates/sensormodelpressure.json
+curl -L https://raw.githubusercontent.com/Jscholtes128/Azure-FarmBeatsLabModule/master/Set-Up/templates/sensormodeltemp.json > templates/sensormodeltemp.json
 
 python3 sensorregistration.py --endpoint $ENDPOINT--client_id $CLIENT_ID --tenent_id $CLIENT_SECRET --client_secret $TENANT_ID
