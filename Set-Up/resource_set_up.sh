77389f5dfea4a4dea38928cf67576235e173377f
Jonathan Scholtes <joscholt@microsoft.com>
2020-02-06 16:49:38 -0600

    bug testing

--- Set-Up/resource_set_up.sh
+++ Set-Up/resource_set_up.sh
@@ -19,7 +19,8 @@ az iot edge set-modules --device-id $DEVICE_ID --hub-name $IOT_HUB --content dep
 
 rm deployment.arm32v7.json
 
-CONNECTION_STR=`az iot hub device-identity show-connection-string --device-id "$DEVICE_ID" --hub-name "$IOT_HUB"`
+CONNECTION_STR_JSON=`az iot hub device-identity show-connection-string --device-id "$DEVICE_ID" --hub-name "$IOT_HUB"`
+CONNECTION_STR=`jq '.Instances[0].connectionString' "$CONNECTION_STR_JSON"`
 sleep 10s
 echo "Connection String: ${CONNECTION_STR}"
 
@@ -45,7 +46,7 @@ rm iotedgeinstall.sh
 
 end=`date -u -d "240 minutes" '+%Y-%m-%dT%H:%MZ'`
 SAS=`az storage blob generate-sas --account-name "$STORE" -c install -n iotedgeinstall.sh --permissions r --expiry "$end" --https-only --full-uri`
-echo "${SAS}"
+echo "SAS: ${SAS}"
 
 TINYURL=$(curl -L https://tinyurl2.azurewebsites.net/api/TinyUrl?url=$SAS)
 
